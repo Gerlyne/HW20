2bda140e6fb5fa2d30710389e02331747cc47d38
Gerlyne <gerlynepaul2@gmail.com>
2021-02-17 21:56:14 -0500

    HW20

---
+++ js/index.js
@@ -0,0 +1,26 @@
+
+var image =document.getElementById ("image")
+image.addEventListener("mouseover", function(){
+    this.style ="box-shadow: 20px 20px 20px aqua";
+    this.width = "110"
+
+});
+
+image.addEventListener("mouseout", function(){
+    this.style ="";
+    this.width = "100"
+
+});
+
+var image2 =document.getElementById ("image2")
+image2.addEventListener("mouseover", function(){
+    this.style ="box-shadow: 20px 20px 20px aqua";
+    this.width = "110"
+
+});
+
+image2.addEventListener("mouseout", function(){
+    this.style ="";
+    this.width = "100"
+
+});
